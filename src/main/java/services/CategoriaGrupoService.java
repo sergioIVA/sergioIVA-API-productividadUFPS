b78a5d0c93297372821e1c5d56ac432fa4a92e0a
sergio <sertioivan02@gmail.com>
2018-11-17 21:26:34 -0500

    estructura de categoriaGrupo

--- src/main/java/services/CategoriaGrupoService.java
+++ src/main/java/services/CategoriaGrupoService.java
@@ -1,9 +1,31 @@
 package services;
 
+import model.CategoriaGrupo;
+
 public class CategoriaGrupoService {
 
 	public CategoriaGrupoService() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public Object getCategoriasGrupo() {
+	      return null;	
+	}
+	
+	public CategoriaGrupo getCategoriaGrupo(int id) {
+		return null;
+	}
+	
+	public CategoriaGrupo createCategoriaGrupo(String nombre) {
+		  return null;
+	}
+	
+	public CategoriaGrupo updateCategoriaGrupo(int id) {
+		return null;
+	}
+	
+	public boolean deleteCategoriaGrupo() {
+		  return true;
 	}
 
 }
