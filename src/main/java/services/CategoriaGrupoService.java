f85b1005622b1d1fa8f5db89e0c18e863873a288
sergio <sertioivan02@gmail.com>
2018-11-17 23:03:14 -0500

    service categoria_grupo

--- src/main/java/services/CategoriaGrupoService.java
+++ src/main/java/services/CategoriaGrupoService.java
@@ -1,31 +1,39 @@
 package services;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import Dao.CategoriaGrupoDao;
 import model.CategoriaGrupo;
 
 public class CategoriaGrupoService {
+	
+	final CategoriaGrupoDao categoriaGrupoDao=new CategoriaGrupoDao() ;  
 
-	public CategoriaGrupoService() {
+	public CategoriaGrupoService(){
 		// TODO Auto-generated constructor stub
 	}
 	
-	public Object getCategoriasGrupo() {
-	      return null;	
+	public Object getCategoriasGrupo()throws Exception {
+		Map<String, Object> map = new HashMap<String, Object>();
+	      return map.put("categoria_grupo",this.categoriaGrupoDao.getCategoriaGrupos());	
 	}
 	
-	public CategoriaGrupo getCategoriaGrupo(int id) {
-		return null;
+	public CategoriaGrupo getCategoriaGrupo(int id)throws Exception {
+		return this.categoriaGrupoDao.getCategoriaGrupo(id);
 	}
 	
-	public CategoriaGrupo createCategoriaGrupo(String nombre) {
-		  return null;
+	public CategoriaGrupo createCategoriaGrupo(String nombre)throws Exception {
+		  return this.categoriaGrupoDao.createCategoriaGrupo(nombre);
 	}
 	
-	public CategoriaGrupo updateCategoriaGrupo(int id) {
-		return null;
+	public CategoriaGrupo updateCategoriaGrupo(CategoriaGrupo g,String nombre)throws Exception {
+		return this.categoriaGrupoDao.updateCategoriaGrupo(g, nombre);
 	}
 	
-	public boolean deleteCategoriaGrupo() {
-		  return true;
+	public boolean deleteCategoriaGrupo(int id)throws Exception {
+		  return this.categoriaGrupoDao.deleteFacultad(id);
 	}
 
 }
