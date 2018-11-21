a5d7983495143eef318a9768c37fc354694eded4
sandovalf4707 <sandovalf4707@gmail.com>
2018-11-21 16:13:17 -0500

    nuevo

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -97,7 +97,7 @@ public class MainController {
 			get("/datosSemilleroDirector",(req,res)->{return this.procesoEspecificoController.getSemilleroDirector(req, res);},new JsonTransformer());
 			get("/lineaGrupoDocenteGrupo/:idGrupo",(req,res)->{return this.procesoEspecificoController.getLineaGrupoDocenteGrupo(req, res);},new JsonTransformer());
 			get("/proyectoResponsable",(req,res)->{return this.procesoEspecificoController.getProyectoResponsable(req, res);},new JsonTransformer());
-			get("/lineasGrupoTipoProyectoGrupo/:tipoSession&:idGrupoSemillero",(req,res)->{return this.procesoEspecificoController.getLineasGrupoTipoProyectoGrupo(req, res);},new JsonTransformer());
+			get("/lineasGrupoTipoProyectoGrupo/:tipoSession/session/:dGrupoSemillero",(req,res)->{return this.procesoEspecificoController.getLineasGrupoTipoProyectoGrupo(req, res);},new JsonTransformer());
 			
 			//semillero
 			post("/semillero",(req,res)->{return this.semilleroController.createSemillero(req, res);},new JsonTransformer());
